53e9cc5b1083a419d3d94bbfa9efcfb79588e5ce
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2016-03-27 20:58:36 +0000

    add server and package files

---
+++ server.js
@@ -0,0 +1,75 @@
+var http = require('http')
+var url = require('url')
+var strtime = require('strftime')
+
+
+
+
+
+var server = http.createServer(function (req, res) {
+    var parsedURL = url.parse(req.url, true)
+    var dateInfo = new Date(parsedURL.path.replace('/','').replace(/%20/g, ' '))
+    
+
+    var unix = 0
+    var natural = ''
+    
+   
+    
+    if (isNaN(dateInfo) == false) {
+    
+        unix = dateInfo.getTime(),
+        natural = strtime('%B %d, %Y', dateInfo)
+    
+         var returnObject = {
+            'unix': unix, 
+            'natural': natural
+        
+        }
+    
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(returnObject))
+    }
+    
+    
+    
+    else {
+        
+        dateInfo = new Date(parsedURL.path.replace('/','').replace(/%20/g, ' '))
+        
+        if (dateInfo == 'Invalid Date') {
+            unix = null
+            natural = null
+         
+         
+         var returnObject = {
+            'unix': unix, 
+            'natural': natural
+        
+            }
+    
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(returnObject))   
+       
+        }
+        
+        if (dateInfo != 'Invalid Date') {
+        unix = dateInfo.getTime()
+        natural = strtime('%B %d, %Y', unix)
+        
+         var returnObject = {
+            'unix': unix, 
+            'natural': natural
+        
+            }
+    
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(returnObject))
+    }
+ 
+    }   
+
+})
+
+server.listen(8080)
+
